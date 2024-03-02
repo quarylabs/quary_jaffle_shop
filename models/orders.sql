f17b9f81b42606021869c3692c24c0741bbf58c8
Louis J <132601011+louisjoecodes@users.noreply.github.com>
2024-03-02 09:52:39 -0800

    update orders

--- models/orders.sql
+++ models/orders.sql
@@ -1,56 +1,33 @@
-{% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
-
 with orders as (
-
     select * from q.stg_orders
-
 ),
-
 payments as (
-
     select * from q.stg_payments
-
 ),
-
 order_payments as (
-
     select
         order_id,
-
-        {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
-        {% endfor -%}
-
+        sum(case when payment_method = 'credit_card' then amount else 0 end) as credit_card_amount,
+        sum(case when payment_method = 'coupon' then amount else 0 end) as coupon_amount,
+        sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount,
+        sum(case when payment_method = 'gift_card' then amount else 0 end) as gift_card_amount,
         sum(amount) as total_amount
-
     from payments
-
     group by order_id
-
 ),
-
 final as (
-
     select
         orders.order_id,
         orders.customer_id,
         orders.order_date,
         orders.status,
-
-        {% for payment_method in payment_methods -%}
-
-        order_payments.{{ payment_method }}_amount,
-
-        {% endfor -%}
-
+        order_payments.credit_card_amount,
+        order_payments.coupon_amount,
+        order_payments.bank_transfer_amount,
+        order_payments.gift_card_amount,
         order_payments.total_amount as amount
-
     from orders
-
-
     left join order_payments
         on orders.order_id = order_payments.order_id
-
 )
-
-select * from final
+select * from final;
